9a39086c755015c470474ed87d040d35d7f4416d
Martín Medina <tinnmedina@gmail.com>
2018-10-18 19:08:01 -0300

    Fix file format

--- rskj-core/src/main/java/org/ethereum/config/net/TestNetConfig.java
+++ rskj-core/src/main/java/org/ethereum/config/net/TestNetConfig.java
@@ -22,6 +22,7 @@ package org.ethereum.config.net;
 
 import org.ethereum.config.blockchain.testnet.TestNetAfterBridgeSyncConfig;
 import org.ethereum.config.blockchain.testnet.TestNetBeforeBridgeSyncConfig;
+import org.ethereum.config.blockchain.testnet.TestNetDifficultyDropEnabledConfig;
 
 public class TestNetConfig extends AbstractNetConfig {
     public static final TestNetConfig INSTANCE = new TestNetConfig();
@@ -30,6 +31,7 @@ public class TestNetConfig extends AbstractNetConfig {
         add(0, new TestNetBeforeBridgeSyncConfig());
         // 21 days of 1 block every 14 seconds.
         // On blockchain launch blocks will be faster until difficulty is adjusted to available hashing power.
-        add(129600, new TestNetAfterBridgeSyncConfig());
+        add(129_600, new TestNetAfterBridgeSyncConfig());
+        add(501_726, new TestNetDifficultyDropEnabledConfig());
     }
 }
